f4d995d04164730ead5cf25c0585e92493855ef8
Eduard Meza <eduardmesa@unisabana.edu.co>
2026-02-05 19:18:59 -0500

    ..

--- src/main/java/com/mycompany/uml_arqui/UML_Arqui.java
+++ src/main/java/com/mycompany/uml_arqui/UML_Arqui.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.uml_arqui;
 
-/**
- *
- * @author ASUS
- */
 public class UML_Arqui {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mycompany/uml_arqui/classes/absatracts/Persona.java
@@ -0,0 +1,5 @@
+package com.mycompany.uml_arqui.classes.absatracts;
+
+public abstract class Persona {
+    
+}
